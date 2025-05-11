3574e69802ebd03b83a1ab8ace14379a5a16576d
Jackal_lzt <bkyarokun@gmail.com>
2025-05-11 16:16:08 +0800

    feat: stagger property

--- gsap-1.js
+++ gsap-1.js
@@ -40,3 +40,26 @@ gsap.fromTo(
 		yoyo: true,
 	}
 );
+
+// stagger - 所有选中的元素一次执行动画
+gsap.to(".fred-stagger-edges", {
+	stagger: {
+		each: 0.3, // 每个元素执行动画前的间隔
+		// amount: 2, // 整体执行时间为2秒，比如有5个元素，amount为2时，开始到结束花费2秒，每个元素执行动画的时间为2/5=0.4秒
+		from: "edges", // "start" "center" "end" "edges" 开始的位置，center从中间向两边扩散, edges从两边向中间扩散
+	},
+	y: -100,
+	repeat: -1,
+	delay: 1,
+});
+gsap.to(".fred-stagger-center", {
+	stagger: {
+		each: 0.3, // 每个元素执行动画前的间隔
+		// amount: 2, // 整体执行时间为2秒，比如有5个元素，amount为2时，开始到结束花费2秒，每个元素执行动画的时间为2/5=0.4秒
+		from: "center", // "start" "center" "end" "edges" 开始的位置，center从中间向两边扩散, edges从两边向中间扩散
+	},
+	y: 100,
+	repeat: -1,
+	delay: 1,
+});
+
